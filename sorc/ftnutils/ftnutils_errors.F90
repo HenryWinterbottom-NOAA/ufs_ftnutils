c9d8e2a3e511139588a1580b006d9b5c1cf4c0c7
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-07-30 14:09:24 -0600

    Update errors interface.

--- sorc/ftnutils/ftnutils_errors.F90
+++ sorc/ftnutils/ftnutils_errors.F90
@@ -1,32 +1,32 @@
-!> @file: ftnutils_errors.F90
-!! @details: This module contains the base-class object for all
-!!           exception classes.
-!! @author: Henry R. Winterbottom
-!! @date: 02 July 2023
-!! @version: 0.0.1
-!! @license: LGPL v2.1
+! > @file sorc/ftnutils/ftnutils_errors.F90
+! ! @details This module contains the base-class object for all
+! !          exception classes.
+! ! @author Henry R. Winterbottom
+! ! @date 02 July 2023
+! ! @version 0.0.1
+! ! @license LGPL v2.1
 module ftnutils_errors
-   use ftnutils_kinds, only: maxchar
-   use ftnutils_log, only: Logger
-   implicit none
-   private
+    use ftnutils_kinds, only: maxchar
+    use ftnutils_log, only: logger
+    implicit none
+    private
 
-   !> @brief: The Error base-class object.
-   type, public :: Error
-      type(Logger) :: logobj
-   contains
-      procedure, public :: raise
-   end type Error
+    ! > @brief The Error base-class object.
+    type, public :: Error
+    type(logger) :: logobj
+contains
+    procedure, public :: raise
+    end type Error
 contains
 
-   !> @brief: Raises the respective exception.
-  !! @param: msg
-  !!    - The string to accompany the respective exception.
-   subroutine raise(this, msg)
-      class(Error) :: this
-      character(len=maxchar) :: msg
+    ! > @brief Raises the respective exception.
+    ! ! @param msg
+    ! !    - The string to accompany the respective exception.
+    subroutine raise(this, msg)
+        class(error), intent(in) :: this
+        character(len=maxchar) :: msg
 
-      call this%logobj%error(msg=msg)
-      stop
-   end subroutine raise
+        call this%logobj%error(msg = msg)
+        stop
+    end subroutine raise
 end module ftnutils_errors
